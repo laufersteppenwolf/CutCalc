6c14850ce0747b7a35df3c4785644d4204cc9ccd
laufersteppenwolf <laufersteppenwolf@gmail.com>
2015-03-05 22:44:09 +0100

    Initial push

---
+++ src/main/java/com/laufersteppenwolf/cutcalc/MainActivity.java
@@ -0,0 +1,158 @@
+package com.laufersteppenwolf.cutcalc;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+public class MainActivity extends Activity {
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public CharSequence getRpm() {
+        final TextView mRpm =
+                (TextView) findViewById(R.id.rpm);
+        return mRpm.getText();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonCalc = (Button) findViewById(R.id.buttonCalc);
+        final Button buttonDrilling = (Button) findViewById(R.id.buttonDrilling);
+        final Button buttonMilling = (Button) findViewById(R.id.buttonMilling);
+        final Button buttonTurning = (Button) findViewById(R.id.buttonTurning);
+
+        final Switch pvcSwitch = (Switch) findViewById(R.id.switchPvc);
+
+        final TextView mDiameter =
+                (TextView) findViewById(R.id.diameter);
+        final TextView mVc =
+                (TextView) findViewById(R.id.cuttingSpeed);
+        final TextView mRpm =
+                (TextView) findViewById(R.id.rpm);
+        final TextView mRpmFeed =
+                (TextView) findViewById(R.id.rpmFeed);
+
+        final RelativeLayout mRelLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+//        mRelLayout.setBackgroundResource(R.drawable.schaftfraeser_scaled);
+
+        final Animation shakeX = AnimationUtils.loadAnimation(this, R.anim.shake_x);
+
+        pvcSwitch.setChecked(false); // Don't use PVC data by default
+
+        mRpm.setText("0");
+
+        buttonCalc.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int multiplier = getResources().getInteger(R.integer.multiplier_switch);
+                        double vc = Double.parseDouble(mVc.getText().toString());
+                        double diameter = Double.parseDouble(mDiameter.getText().toString());
+
+                        if (diameter == 0 ) {
+                            mDiameter.setBackgroundColor(Color.RED);
+                            buttonCalc.startAnimation(shakeX);
+                            return;
+                        } else {
+                            mDiameter.setBackgroundColor(Color.TRANSPARENT);
+                        }
+
+                        double result = round((vc * 1000)/(Math.PI * diameter),2);
+
+                        if (pvcSwitch.isChecked()) {
+                            result = multiplier * result;
+                        }
+                        mRpm.setText(Double.toString(result));
+                    }
+                }
+        );
+        buttonDrilling.setOnClickListener(
+                new Button.OnClickListener()  {
+                    @Override
+                    public void onClick(View v) {
+                        mVc.setText(R.string.string_vc_drilling);
+                        mRelLayout.setBackgroundResource(R.drawable.bohrer_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+        buttonMilling.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mVc.setText(R.string.string_vc_milling);
+                        mRelLayout.setBackgroundResource(R.drawable.schaftfraeser_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+        buttonTurning.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mVc.setText(R.string.string_vc_turning);
+                        mRelLayout.setBackgroundResource(R.drawable.drehmeissel_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_features, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        final Intent intent = new Intent(MainActivity.this, feedRateActivity.class);
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_features_feedRate:
+                intent.putExtra("RPM", getRpm().toString());
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ src/main/java/com/laufersteppenwolf/cutcalc/feedRateActivity.java
@@ -0,0 +1,143 @@
+package com.laufersteppenwolf.cutcalc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+public class feedRateActivity extends Activity {
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_rate);
+
+        final Intent intent = getIntent();
+
+        final Button buttonCalc = (Button) findViewById(R.id.buttonCalcFeed);
+        final Button buttonDrilling = (Button) findViewById(R.id.buttonDrillingFeed);
+        final Button buttonMilling = (Button) findViewById(R.id.buttonMillingFeed);
+        final Button buttonTurning = (Button) findViewById(R.id.buttonTurningFeed);
+
+        final Switch pvcSwitch = (Switch) findViewById(R.id.switchPvcFeed);
+
+        final TextView mRpm =
+                (TextView) findViewById(R.id.rpmFeed);
+        final TextView mFeed =
+                (TextView) findViewById(R.id.feed);
+        final TextView mBlades =
+                (TextView) findViewById(R.id.bladesFeed);
+        final TextView mFeedRate =
+                (TextView) findViewById(R.id.feedRate);
+        final String mRpmRpm =
+                intent.getStringExtra("RPM");
+
+        final RelativeLayout mRelLayout = (RelativeLayout) findViewById(R.id.relativeLayoutFeed);
+        mRelLayout.setBackgroundResource(R.drawable.schaftfraeser_scaled);
+
+        pvcSwitch.setChecked(false); // Don't use PVC data by default
+        mRpm.setText(mRpmRpm);       // Use the RPM from the previously calculated data
+
+        buttonCalc.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int multiplier = getResources().getInteger(R.integer.multiplier_switch);
+                        double rpm = Double.parseDouble(mRpm.getText().toString());
+                        double feed = Double.parseDouble(mFeed.getText().toString());
+                        double blades = Double.parseDouble(mBlades.getText().toString());
+
+/*                        if (diameter == 0 ) {
+                            mDiameter.setBackgroundColor(Color.RED);
+                            buttonCalc.startAnimation(shakeX);
+                            return;
+                        } else {
+                            mDiameter.setBackgroundColor(Color.TRANSPARENT);
+                        } */ //TODO: Add warnings and NPE preventions
+
+                        double result = round(rpm * feed * blades,2);
+
+                        if (pvcSwitch.isChecked()) {
+                            result = multiplier * result;
+                        }
+                        mFeedRate.setText(Double.toString(result));
+                    }
+                }
+        );
+        buttonDrilling.setOnClickListener(
+                new Button.OnClickListener()  {
+                    @Override
+                    public void onClick(View v) {
+                        mBlades.setText(R.string.string_default_blades_drilling);
+                        mRelLayout.setBackgroundResource(R.drawable.bohrer_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+        buttonMilling.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mBlades.setText(R.string.string_default_blades_milling);
+                        mRelLayout.setBackgroundResource(R.drawable.schaftfraeser_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+        buttonTurning.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mBlades.setText(R.string.string_default_blades_turning);
+                        mRelLayout.setBackgroundResource(R.drawable.drehmeissel_scaled);
+                        buttonCalc.performClick();
+                    }
+                }
+        );
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_feed_rate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
